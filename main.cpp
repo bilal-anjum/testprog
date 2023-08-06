221e56f79b94f39f9f53a5b7c21d693293cd7cea
bilal-anjum <bilal.anjum@gmail.com>
2023-08-06 22:21:37 +0500

    Using testlib in testbin

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include <mathfuncs.h>
+
 int main() {
     std::cout << "Hello World" << std::endl;
+    std::cout << "Result of addition is: " << MathFuncs::add(1, 1) << std::endl;
     
     return 0;
 }
